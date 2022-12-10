a197e992aec8402cb8d6b33a192ed831f924bf11
han <hoang8129@gmail.com>
2022-12-10 23:12:45 +0700

    Copies dropdown menu template

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,16 +1,17 @@
-function myFunction() {
-      document.getElementById("myDropdown").classList.toggle("show");
+function dropdown() {
+      document.getElementById("myDropdown").classList.toggle("show")
 }
 
 window.onclick = function(event) {
       if (!event.target.matches('.dropdown-button')) {
-            let dropdowns = document.getElementsByClassName("dropdown-content");
-            
-            for (let i = 0; i < dropdowns.length; i++) {
-                  let openDropdown = dropdowns[i];
-                  if (openDropdown.classList.contains('show')) {
-                        openDropdown.classList.remove('show');
-                  }
-            }
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+
+        for ( var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
       }
-}
+    }
