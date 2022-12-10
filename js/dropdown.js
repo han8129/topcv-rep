07d776ae0180de0eb608487fcbd8803924092d98
han <hoang8129@gmail.com>
2022-12-10 14:55:57 +0700

    Tries adding JavaScript

---
+++ js/dropdown.js
@@ -0,0 +1,16 @@
+function myFunction() {
+      document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+      if (!event.target.matches('.dropdown-button')) {
+            let dropdowns = document.getElementsByClassName("dropdown-content");
+            
+            for (let i = 0; i < dropdowns.length; i++) {
+                  let openDropdown = dropdowns[i];
+                  if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                  }
+            }
+      }
+}
