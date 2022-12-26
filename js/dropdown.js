c5204d500f22d39ec711cb306e7c243be959960a
han <hoang8129@gmail.com>
2022-12-27 00:04:19 +0700

    add dropdown menu

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,17 +1,37 @@
-function dropdown() {
-      document.getElementById("myDropdown").classList.toggle("show")
+function dropdown(name) {
+	document.getElementById(name).classList.toggle("show");
+
+	let dropDownIsTrue;
+
 }
 
-window.onclick = function(event) {
-      if (!event.target.matches('.dropdown-button')) {
+window.onload = function () {
+	let dropDownMenuList = document.getElementsByClassName("dropdown-menu");
 
-        var dropdowns = document.getElementsByClassName("dropdown-content");
+	let indexSingleMenu;
+	for (indexSingleMenu = 0; indexSingleMenu < dropDownMenuList.length; indexSingleMenu++) {
+		let singleMenu = dropDownMenuList[indexSingleMenu].children;
 
-        for ( var i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
-        }
-      }
-    }
+		let menuToggleButton = singleMenu[0];
+		menuToggleButton.addEventListener("click", function () {
+			singleMenu[1].classList.toggle("show");
+		}
+		)
+	}
+}
+// add this event listener to particular elements
+window.onclick = function (event) {
+	if (!event.target.matches(".dropdown__button")) {
+		let dropDowns = document.getElementsByClassName("dropdown__content");
+
+		let i;
+		for (i = 0; i < dropDowns.length; i++) {
+			let openDropdown = dropDowns[i];
+
+			if (openDropdown.classList.contains("show")) {
+				openDropdown.classList.remove("show");
+			}
+		}
+	}
+
+}
