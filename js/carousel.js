831aae9c1ccb85047559eab2fe42b06c40261c29
han <hoang8129@gmail.com>
2022-12-29 01:08:40 +0700

    Finalize carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,19 +1,23 @@
-const carousel =  document.querySelector(".carousel");
+const carouselsArray =  document.querySelectorAll(".carousel");
 
-const track = carousel.querySelector(".carousel__track");
-const slidesArray = Array.from(track.children);
+let carouselIndex;
+for (carouselIndex = 0; carouselIndex < carouselsArray.length; carouselIndex++)
+{
+       const carousel = carouselsArray[carouselIndex];
+       const track = carousel.querySelector(".carousel__track");
 
-const rightButton = carousel.querySelector(".carousel__button_right");
-const leftButton = carousel.querySelector(".carousel__button_left");
+       const rightButton = carousel.querySelector(".carousel__button_right");
+       const leftButton = carousel.querySelector(".carousel__button_left");
 
-const singleSlideWidth = slidesArray[0].getBoundingClientRect().width;
-const gapWidth = window.getComputedStyle(track).getPropertyValue("gap")
-const amountToMove = singleSlideWidth + parseFloat(gapWidth);
+       const singleSlideWidth = track.querySelector(".carousel__slide").getBoundingClientRect().width;
+       const gapWidth = window.getComputedStyle(track).getPropertyValue("gap")
+       const amountToMove = singleSlideWidth + parseFloat(gapWidth);
 
-rightButton.addEventListener("click", e => {
-       track.scrollBy({top: 0, left: amountToMove, behavior: "smooth"})
-})
+       rightButton.addEventListener("click", e => {
+              track.scrollBy({ top: 0, left: amountToMove, behavior: "smooth" })
+       })
 
-leftButton.addEventListener("click",e => {
-       track.scrollBy({top: 0, left: - amountToMove, behavior: "smooth"})
-})
+       leftButton.addEventListener("click", e => {
+              track.scrollBy({ top: 0, left: - amountToMove, behavior: "smooth" })
+       })
+}
